2783bfdedb2f3ceac8f9756d736b048f1b3651b3
Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
2024-12-12 01:58:37 -0600

    it is working 🥹

--- up.sh
+++ up.sh
@@ -2,5 +2,6 @@
 
 set -euo pipefail
 
+cloud-init status --wait --long
 sudo docker compose up -d
 curl -f "http://localhost:11434/api/pull" -d '{"name": "llama3.3"}'
--- user_data.sh
+++ user_data.sh
@@ -75,6 +75,21 @@ install_nvidia_driver() {
     apt-get install -y nvidia-open
 }
 
+# https://github.com/ollama/ollama/blob/main/docs/docker.md
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#installing-the-nvidia-container-toolkit
+install_nvidia_container_toolkit() {
+    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
+        | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+    curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
+        | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
+        | tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    apt-get update -y
+    apt-get install -y nvidia-container-toolkit
+    nvidia-ctk runtime configure --runtime=docker
+    systemctl restart docker
+}
+
 mount_ebs_volume
 install_docker
 install_nvidia_driver
+install_nvidia_container_toolkit
