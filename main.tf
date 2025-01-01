f24327e2e792f342944e9bb0c8b5dc52a224b730
Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
2025-01-01 02:16:59 -0600

    use persistent spot instance type

--- main.tf
+++ main.tf
@@ -93,6 +93,10 @@ resource "aws_instance" "llm_server" {
 
   instance_market_options {
     market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type             = "persistent"
+    }
   }
 }
 
